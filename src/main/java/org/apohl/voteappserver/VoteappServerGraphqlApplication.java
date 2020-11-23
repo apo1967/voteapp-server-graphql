4a552a68a3ee4c76ff0925e1892d9ce9bc68be21
apo1967 <apo1967@gmx.de>
2020-11-23 14:36:17 +0100

    * Hint on possibly broken mapping * Dockerfile

--- src/main/java/org/apohl/voteappserver/VoteappServerGraphqlApplication.java
+++ src/main/java/org/apohl/voteappserver/VoteappServerGraphqlApplication.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The Spring Boot application
+ */
 @SpringBootApplication
 @RestController
 public class VoteappServerGraphqlApplication {
@@ -14,6 +17,13 @@ public class VoteappServerGraphqlApplication {
         SpringApplication.run(VoteappServerGraphqlApplication.class, args);
     }
 
+    /**
+     * Just for testing. Please note, that the mapping won't work if GraphQL is mapped to the root path "/" because
+     * then the "/ping" request will never reach this controller.
+     *
+     * @param name will be passed back to the client in a welcome string
+     * @return the response json
+     */
     @GetMapping(value = {"/ping"})
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
